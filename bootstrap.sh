522d43ce05961a1d2e6d9a9b6b48c90d23de4777
Nathalie Casati <nathalie.casati@chuv.ch>
2024-03-08 14:48:14 +0100

    improve bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+FQDN="chorus-tre.ch"
+
 # install argocd
 helm dep update charts/argo-cd
 kubectl get namespace | grep -q "^argocd " || kubectl create namespace argocd
-helm install chorus-build-argo-cd charts/argo-cd -n argocd
+helm install chorus-build-argo-cd charts/argo-cd -n argocd --set argo-cd.server.ingress.hosts[0]=argo-cd.build.$FQDN --set argo-cd.server.ingressGrpc.hosts[0]=grpc.argo-cd.build.$FQDN
 echo "" 
 
 # install caddy-ingress-controller
@@ -14,10 +16,11 @@ echo ""
 
 # install registry
 kubectl get namespace | grep -q "^registry " || kubectl create namespace registry
-helm install chorus-build-registry charts/registry -n registry
+helm install chorus-build-registry charts/registry -n registry --set ingress.hosts[0]=registry.build.$FQDN
 echo "" 
 
 # wait
+echo "Waiting for pods to be ready..."
 kubectl wait pod \
 	--all \
 	--for=condition=Ready \
@@ -35,10 +38,18 @@ kubectl wait pod \
         --timeout=60s
 echo "" 
 
+
 # get argocd initial password
+echo "ArgoCD username: admin"
 echo -n "ArgoCD password: "
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-echo -e "\n" 
+echo -e "\n"
+
+# display ArgoCD URL
+echo -e "ArgoCD is available at: https://argo-cd.build.$FQDN\n"
+
+# display OCI Registry URL
+echo -e "OCI Registry is available at: https://registry.build.$FQDN\n"
 
 # create namespace for launching argo-workflows
 kubectl get namespace | grep -q "^argo " || kubectl create namespace argo
