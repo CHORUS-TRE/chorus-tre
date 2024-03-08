42962684c6674e040212c69c210c38152c3c77ed
Nathalie Casati <nathalie.casati@chuv.ch>
2024-03-08 13:45:03 +0100

    improve bootstrap.sh namespace creation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 # install argocd
+helm dep update charts/argo-cd
+kubectl get namespace | grep -q "^argocd " || kubectl create namespace argocd
 helm install chorus-build-argo-cd charts/argo-cd -n argocd
 echo "" 
 
 # install caddy-ingress-controller
+helm dep update charts/caddy-ingress-controller
+kubectl get namespace | grep -q "^caddy-system " || kubectl create namespace caddy-system
 helm install chorus-build-caddy-ingress-controller charts/caddy-ingress-controller -n caddy-system
 echo "" 
 
 # install registry
+kubectl get namespace | grep -q "^registry " || kubectl create namespace registry
 helm install chorus-build-registry charts/registry -n registry
 echo "" 
 
@@ -36,7 +41,7 @@ kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.pas
 echo -e "\n" 
 
 # create namespace for launching argo-workflows
-kubectl create namespace argo
+kubectl get namespace | grep -q "^argo " || kubectl create namespace argo
 
 # deploy the ApplicationSet
 kubectl -n argocd apply -f deployment/applicationset/applicationset-chorus.yaml
