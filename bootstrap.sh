e97bf8b95a4de5009d397ed456f4ab03558e06db
Martin Fontanet <martin.fontanet@epfl.ch>
2024-07-30 13:41:22 +0200

    chore: run shfmt

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,12 +5,12 @@ set -e
 
 read -p "Please enter domain name of your CHORUS instance: " DOMAIN_NAME
 if [[ -z "$DOMAIN_NAME" ]]; then
-  DOMAIN_NAME="chorus-tre.ch"
+	DOMAIN_NAME="chorus-tre.ch"
 fi
 
 read -p "Please enter your Let's Encrypt email: " EMAIL
 if [[ -z "$EMAIL" ]]; then
-  EMAIL="no-reply@chorus-tre.ch"
+	EMAIL="no-reply@chorus-tre.ch"
 fi
 
 # install ingress-nginx
@@ -22,42 +22,42 @@ kubectl wait pod \
 	--all \
 	--for=condition=Ready \
 	--namespace=ingress-nginx \
-    --timeout=60s
+	--timeout=60s
 
 # install cert-manager
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.5/cert-manager.crds.yaml
 helm dep update charts/cert-manager
 helm install chorus-build-cert-manager charts/cert-manager -n cert-manager --create-namespace --set clusterissuer.email=$EMAIL
-echo "" 
+echo ""
 echo "Waiting for cert-manager..."
 kubectl wait pod \
-    --all \
-    --for=condition=Ready \
-    --namespace=cert-manager \
-    --timeout=60s
+	--all \
+	--for=condition=Ready \
+	--namespace=cert-manager \
+	--timeout=60s
 
 # install argocd
 kubectl apply -k "https://github.com/argoproj/argo-cd/manifests/crds?ref=v2.11.2"
 helm dep update charts/argo-cd
 helm install chorus-build-argo-cd charts/argo-cd -n argocd --create-namespace --set argo-cd.global.domain=argo-cd.build.$DOMAIN_NAME --set argo-cd.server.ingress.extraTls[0].hosts[0]=argo-cd.build.$DOMAIN_NAME --set argo-cd.server.ingress.extraTls[0].secretName=argocd-ingress-http --set argo-cd.server.ingressGrpc.extraTls[0].hosts[0]=grpc.argo-cd.build.$DOMAIN_NAME --set argo-cd.server.ingressGrpc.extraTls[0].secretName=argocd-ingress-grpc
-echo "" 
+echo ""
 echo "Waiting for argo-cd..."
 kubectl wait pod \
 	--all \
 	--for=condition=Ready \
 	--namespace=argocd \
 	--selector app.kubernetes.io/name!=argocd-redis-secret-init \
-  --timeout=60s
+	--timeout=60s
 
 # install registry
 helm install chorus-build-registry charts/registry -n registry --create-namespace --set ingress.hosts[0]=registry.build.$DOMAIN_NAME --set ingress.tls[0].hosts[0]=registry.build.$DOMAIN_NAME --set ingress.tls[0].secretName=registry-tls
-echo "" 
+echo ""
 echo "Waiting for registry..."
 kubectl wait pod \
 	--all \
 	--for=condition=Ready \
 	--namespace=registry \
-    --timeout=60s
+	--timeout=60s
 
 # install sealed-secrets
 helm dep update charts/sealed-secrets
@@ -65,10 +65,10 @@ helm install chorus-build-sealed-secrets charts/sealed-secrets -n kube-system
 echo ""
 echo "Waiting for sealed-secrets..."
 kubectl wait pod \
-    --for=condition=Ready \
-    --namespace=kube-system \
-    --selector 'app.kubernetes.io/name=sealed-secrets' \
-    --timeout=60s
+	--for=condition=Ready \
+	--namespace=kube-system \
+	--selector 'app.kubernetes.io/name=sealed-secrets' \
+	--timeout=60s
 
 # get argocd initial password
 echo "ArgoCD username: admin"
@@ -86,9 +86,9 @@ echo -e "OCI Registry is available at: https://registry.build.$DOMAIN_NAME\n"
 kubectl -n argocd apply -f deployment/applicationset/applicationset-chorus.yaml
 
 # display DNS records
-ARGOCD_EXTERNAL_IP=$(kubectl -n argocd  get ingress chorus-build-argo-cd-argocd-server -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
-GRPC_ARGOCD_EXTERNAL_IP=$(kubectl -n argocd  get ingress chorus-build-argo-cd-argocd-server-grpc -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
-REGISTRY_EXTERNAL_IP=$(kubectl -n registry  get ingress chorus-build-registry -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
+ARGOCD_EXTERNAL_IP=$(kubectl -n argocd get ingress chorus-build-argo-cd-argocd-server -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
+GRPC_ARGOCD_EXTERNAL_IP=$(kubectl -n argocd get ingress chorus-build-argo-cd-argocd-server-grpc -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
+REGISTRY_EXTERNAL_IP=$(kubectl -n registry get ingress chorus-build-registry -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
 
 echo -e "Please set the following DNS records:\n"
 echo -e "argo-cd.build.$DOMAIN_NAME -> $ARGOCD_EXTERNAL_IP"
