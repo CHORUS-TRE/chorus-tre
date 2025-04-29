f66a483aae845fbd5f6df8d42ec81b2f6936fd4e
Jonathan Haab <jonathan.haab@chuv.ch>
2025-04-29 14:45:15 +0200

    use template for chart_versions.tf

--- deployment/install/scripts/init_helm_charts.sh
+++ deployment/install/scripts/init_helm_charts.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
+debug=false
+if [[ "$1" == "--debug" ]]; then
+  set -x
+  shift
+  debug=true
+fi
+
+echo "Pulling the Helm charts needed by the Terraform scripts "\
+"and copying over their versions..."
+echo "(Use the '--debug' option to see the full trace)"
+
+header_msg="/* This file was automatically generated
+by $0
+DO NOT modify manually
+Any change will be overwritten
+*/"
+tf_file_template="charts_versions.tftpl"
 tf_file="charts_versions.tf"
+touch $tf_file && echo "$header_msg" > $tf_file
+sed '/\/\*/,/\*\//d' $tf_file_template  >> $tf_file
+
 chart_rel_path="../../charts"
 tf_variable_names=$(grep 'variable ' "$tf_file" | sed 's/variable "\([^"]*\)".*/\1/')
 
-echo "Fetching Helm charts versions..."
 for var in $tf_variable_names; do
     # Extract chart name
     # from terraform variable name
@@ -25,5 +44,15 @@ for var in $tf_variable_names; do
     fi
     # Replace default Helm chart version
     sed -i '' -E "/variable \"$var\"[[:space:]]*\{/,/^\}/ s/(default[[:space:]]*=[[:space:]]*)\"[^\"]+\"/\1\"$version\"/" "$tf_file" || exit 1
+    # Pull the chart
+    if [[ $debug == true ]]; then
+        cd $chart_rel_path/$chart_name && \
+        helm dependency update $chart && \
+        cd - || exit 1
+    else
+        cd $chart_rel_path/$chart_name && \
+        helm dependency update $chart > /dev/null 2>&1  && \
+        cd - > /dev/null 2>&1 || exit 1
+    fi
 done
 echo "Done"
