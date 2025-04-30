e707cd0e23d690b208b591ab12bc22d44f256f26
Jonathan Haab <jonathan.haab@chuv.ch>
2025-04-30 17:33:08 +0200

    add provider in modules

---
+++ deployment/install/modules/ingress_nginx/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.36.0"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.17.0"
+    }
+  }
+  # Provider functions require Terraform 1.8 and later.
+  required_version = ">= 1.8.0"
+}
