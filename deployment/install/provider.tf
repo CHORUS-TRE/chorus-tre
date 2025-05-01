450ea1b26906c545e3b196bc5431508e0181e2d7
Jonathan Haab <jonathan.haab@chuv.ch>
2025-05-01 14:55:45 +0200

    add random provider

--- deployment/install/provider.tf
+++ deployment/install/provider.tf
@@ -12,6 +12,10 @@ terraform {
       source = "registry.terraform.io/hashicorp/http"
       version = "3.5.0"
     }
+    random = {
+      source = "registry.terraform.io/hashicorp/random"
+      version = "3.7.2"
+    }
   }
   # Provider functions require Terraform 1.8 and later.
   required_version = ">= 1.8.0"
