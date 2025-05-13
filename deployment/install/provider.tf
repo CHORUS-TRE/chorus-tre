9e3b2f85cd787c93fca16f15e32570ad75a52c26
Jonathan Haab <jonathan.haab@chuv.ch>
2025-05-13 18:27:50 +0200

    add harbor provider

--- deployment/install/provider.tf
+++ deployment/install/provider.tf
@@ -16,6 +16,10 @@ terraform {
       source = "registry.terraform.io/hashicorp/random"
       version = "3.7.2"
     }
+    harbor = {
+      source = "goharbor/harbor"
+      version = "3.10.21"
+    }
   }
   # Provider functions require Terraform 1.8 and later.
   required_version = ">= 1.8.0"
