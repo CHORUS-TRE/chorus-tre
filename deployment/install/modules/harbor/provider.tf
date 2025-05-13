c349702fac82e29e739689664242dd112878ec6e
Jonathan Haab <jonathan.haab@chuv.ch>
2025-05-13 18:25:35 +0200

    fix htpasswd

--- deployment/install/modules/harbor/provider.tf
+++ deployment/install/modules/harbor/provider.tf
@@ -12,6 +12,10 @@ terraform {
       source = "registry.terraform.io/hashicorp/random"
       version = "3.7.2"
     }
+    htpasswd = {
+      source = "loafoe/htpasswd"
+      version = "1.2.1"
+    }
   }
   # Provider functions require Terraform 1.8 and later.
   required_version = ">= 1.8.0"
