0d9c1ff0e7ad79c0166e889967de467f6f1710b4
Jonathan Haab <jonathan.haab@chuv.ch>
2025-05-16 15:37:29 +0200

    create separate output for harbor login url

--- deployment/install/modules/harbor/main.tf
+++ deployment/install/modules/harbor/main.tf
@@ -379,6 +379,11 @@ output "harbor_password" {
 }
 
 output "harbor_url" {
-  value = join("/", [ local.harbor_values_parsed.harbor.externalURL, "account/sign-in" ])
+  value = local.harbor_values_parsed.harbor.externalURL
   description = "Harbor URL"
+}
+
+output "harbor_url_admin_login" {
+  value = join("/", [ local.harbor_values_parsed.harbor.externalURL, "account/sign-in" ])
+  description = "Harbor URL to login with local DB admin user"
 }
