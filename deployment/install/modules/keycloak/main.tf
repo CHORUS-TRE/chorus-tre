0ec52c9b047c884eae0d2235c9317768114935c8
Jonathan Haab <jonathan.haab@chuv.ch>
2025-06-10 16:56:55 +0200

    remove username output

--- deployment/install/modules/keycloak/main.tf
+++ deployment/install/modules/keycloak/main.tf
@@ -130,10 +130,6 @@ output "keycloak_url" {
   value = "https://${local.keycloak_values_parsed.keycloak.ingress.hostname}"
 }
 
-output "keycloak_username" {
-  value = "admin"
-  description = "Keycloak username"
-}
 
 output "keycloak_password" {
   value = data.kubernetes_secret.keycloak_admin_password.data["${local.keycloak_values_parsed.keycloak.auth.passwordSecretKey}"]
